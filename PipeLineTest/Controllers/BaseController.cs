e6c51ca01fe0511410091d1c0518f86f9b486557
Vadim Gez <vadim.gez@gmail.com>
2021-09-05 11:23:00 +0300

    Add project files.

---
+++ PipeLineTest/Controllers/BaseController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace PipeLineTest.Controllers
+{
+    public class BaseController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult GetData()
+        {
+            return Ok("BaseController - GetData - works");
+        }
+    }
+}
---
+++ PipeLineTest/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PipeLineTest.WebApiApplication" Language="C#" %>
